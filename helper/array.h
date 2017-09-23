0184d159d59d9fcab63b3eef9da5043f2da12e6d
doyeon lee <faithdy@kw.ac.kr>
2017-09-23 16:54:52 +0900

    array에 대한 템플릿 적용하였습니다.

--- helper/array.h
+++ helper/array.h
@@ -1,8 +1,10 @@
 #include <iostream>
 #define MAXKEY 4000000
+using namespace std;
 //case 1 : array
 /*templateation need*/
-bool array(int* M1, int* M2) {
+template<typename T>
+bool array(T* M1, T* M2) {
   /*assume one of input parameters has not null.
     because assistant's code is always right.*/
   if(M1 == NULL && M2 == NULL) return true;
--- helper/ref-helper/isomorphicTest.cc
+++ helper/ref-helper/isomorphicTest.cc
@@ -1,20 +1,19 @@
 #include "gtest/gtest.h"
-
-extern bool array(int*, int *);
+#include "../array.h"
 
 TEST(Array,Root_has_null) {
   int validArray[6] = {0,};
 
-  EXPECT_TRUE(array(NULL, NULL));
-  EXPECT_FALSE(array(NULL, validArray));
-  EXPECT_FALSE(array(validArray, NULL));
+  EXPECT_TRUE(array<int>(NULL, NULL));
+  EXPECT_FALSE(array<int>(NULL, validArray));
+  EXPECT_FALSE(array<int>(validArray, NULL));
 }
 
 TEST(Array,has_different_size) {
-  int Array_size_of_6[6] = {1,2,3,4,5,6};
-  int Array_size_of_7[7] = {1,2,3,4,5,6,7};
+  double Array_size_of_6[6] = {1,2,3,4,5,6};
+  double Array_size_of_7[7] = {1,2,3,4,5,6,7};
 
-  EXPECT_FALSE(array(Array_size_of_6, Array_size_of_7));
+  EXPECT_FALSE(array<double>(Array_size_of_6, Array_size_of_7));
 }
 
 TEST(Array,has_different_value) {
@@ -22,15 +21,28 @@ TEST(Array,has_different_value) {
   int Array_multiples_of_2_from_1[6] = {1,2,4,8,16,32};
   int Array_divisors_of_12[6] = {1,2,3,4,6,12};
 
-
-  EXPECT_FALSE(array(Array_1to6, Array_multiples_of_2_from_1));
-  EXPECT_FALSE(array(Array_multiples_of_2_from_1,Array_divisors_of_12));
-  EXPECT_FALSE(array(Array_divisors_of_12,Array_1to6));
+  EXPECT_FALSE(array<int>(Array_1to6, Array_multiples_of_2_from_1));
+  EXPECT_FALSE(array<int>(Array_multiples_of_2_from_1,Array_divisors_of_12));
+  EXPECT_FALSE(array<int>(Array_divisors_of_12,Array_1to6));
 }
 
-TEST(Array,is_isomorphic) {
+TEST(Array,is_isomorphic_case_of_integer) {
   int Array[6] = {1,2,3,4,5,6};
   int Array2[6] = {1,2,3,4,5,6};
 
-  EXPECT_TRUE(array(Array, Array2));
+  EXPECT_TRUE(array<int>(Array, Array2));
+}
+
+TEST(Array,is_isomorphic_case_of_double) {
+  double Array[6] = {1.1,2.2,3.3,4.4,5.5,6.6};
+  double Array2[6] = {1.1,2.2,3.3,4.4,5.5,6.6};
+
+  EXPECT_TRUE(array<int>((int*)Array, (int*)Array2));
+}
+
+TEST(Array,is_isomorphic_case_of_character) {
+  char Array[6] = {'a','b','c','d','e','f'};
+  char Array2[6] = {'a','b','c','d','e','f'};
+
+  EXPECT_TRUE(array<int>((int*)Array, (int*)Array2));
 }
