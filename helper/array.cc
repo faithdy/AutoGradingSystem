8328c66f3deff4f80a4c1cb3b2f54bf8918a2f2f
doyeon lee <faithdy@kw.ac.kr>
2017-09-23 15:33:34 +0900

    자료구조 array에 대한 isomorphic 함수 구현

--- helper/array.cc
+++ helper/array.cc
@@ -1,8 +1,18 @@
 #include <iostream>
+#define MAXKEY 4000000
 //case 1 : array
 /*templateation need*/
 bool array(int* M1, int* M2) {
+  /*assume one of input parameters has not null.
+    because assistant's code is always right.*/
+  if(M1 == NULL && M2 == NULL) return true;
   if(M1 == NULL || M2 == NULL) return false;
+
+  /*also, we set MAXKEY as dummy value. assume
+    each element has key value less than MAXKEY,
+    because we have not input parameter, length */
+  if(*M1 > MAXKEY && *M2 > MAXKEY) return true;
+  if(*M1 > MAXKEY || *M2 > MAXKEY) return false;
 
   if(*M1 != *M2) return false;
 
--- helper/ref-helper/isomorphicTest.cc
+++ helper/ref-helper/isomorphicTest.cc
@@ -2,6 +2,35 @@
 
 extern bool array(int*, int *);
 
-TEST(Isomorphic, Array) {
-  EXPECT_TRUE(!array(NULL, NULL));
+TEST(Array,Root_has_null) {
+  int validArray[6] = {0,};
+
+  EXPECT_TRUE(array(NULL, NULL));
+  EXPECT_FALSE(array(NULL, validArray));
+  EXPECT_FALSE(array(validArray, NULL));
+}
+
+TEST(Array,has_different_size) {
+  int Array_size_of_6[6] = {1,2,3,4,5,6};
+  int Array_size_of_7[7] = {1,2,3,4,5,6,7};
+
+  EXPECT_FALSE(array(Array_size_of_6, Array_size_of_7));
+}
+
+TEST(Array,has_different_value) {
+  int Array_1to6[6] = {1,2,3,4,5,6};
+  int Array_multiples_of_2_from_1[6] = {1,2,4,8,16,32};
+  int Array_divisors_of_12[6] = {1,2,3,4,6,12};
+
+
+  EXPECT_FALSE(array(Array_1to6, Array_multiples_of_2_from_1));
+  EXPECT_FALSE(array(Array_multiples_of_2_from_1,Array_divisors_of_12));
+  EXPECT_FALSE(array(Array_divisors_of_12,Array_1to6));
+}
+
+TEST(Array,is_isomorphic) {
+  int Array[6] = {1,2,3,4,5,6};
+  int Array2[6] = {1,2,3,4,5,6};
+
+  EXPECT_TRUE(array(Array, Array2));
 }
